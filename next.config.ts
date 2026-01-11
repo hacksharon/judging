44c98b9cebdc8832e4fcb4d5bbbe877921fb228c
Thomas Ha <thomasha.1310@gmail.com>
2026-01-11 17:13:51 -0500

    Update indent format

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    basePath: "/judging",
+    reactCompiler: true,
 };
 
 export default nextConfig;
